5f4bf49acf10ab1a70a37d478d070920b84fca3f
Bill Hereth <bhereth@wfrc.org>
2023-09-13 16:43:45 -0600

    Add testing of secondary menu and templates

---
+++ _site-html-menu-list-test/classes/sidebar-item.js
@@ -0,0 +1,140 @@
+// Class for Sidebar Item
+class SidebarItem {
+  constructor(data) {
+      this.id = this.generateIdFromText(data.text);
+      this.text = data.text;
+      this.type = data.type;
+      this.options = data.options;
+      this.selectedOption = data.selectedOption;
+  }
+  
+  generateIdFromText(text) {
+    return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
+  }
+
+  // Render the item based on its type
+  render() {
+    const container = document.createElement('div');
+    container.className = 'sidebar-item';
+    
+    let title = document.createElement("calcite-label");  // Create a new div element
+    title.innerHTML = "<b>" + this.text + "</b>";  // Set its innerHTML
+    container.appendChild(title);  // Append the new element to the container
+
+    // Call a type-specific rendering method
+    this.renderType(container);
+        
+    let space = document.createElement("calcite-label");  // Create a new div element
+    space.innerHTML = "<br/>";  // Set its innerHTML
+    container.appendChild(space);  // Append the new element to the container
+    
+    return container;
+  }
+
+  getSelectedOption() {
+    return this.selectedOption;
+  }
+
+      // Render type-specific content
+  renderType(container) {
+    switch (this.type) {
+      case "radio":
+        this.createRadioButtons(container);
+        break;
+      case "checkbox":
+        this.createCheckBoxes(container);
+        break;
+      case "select":
+        this.createSelect(container);
+        break;
+      // Add more cases for other types as needed
+      default:
+        console.warn(`Unsupported item type: ${this.type}`);
+    }
+  }
+
+  createRadioButtons(container) {
+    this.options.forEach((option, index) => {
+      // Create radio buttons
+      var radioButtonLabel = document.createElement("calcite-label");
+      radioButtonLabel.setAttribute('layout', 'inline');
+      radioButtonLabel.classList.add('pointer-cursor');
+
+      var radioButton = document.createElement("calcite-radio-button");
+      radioButton.name = this.id;
+      radioButton.value = option;
+
+      // Optionally, select the first radio button by default
+      if (option === this.selectedOption) {
+        radioButton.checked = true;
+      }
+
+      // Listen for changes to the radio buttons
+      radioButton.addEventListener("change", (e) => {
+        // to make sure the radio button is the is the actual element
+        const radioButton = e.currentTarget; // or e.target.closest('input[type="radioButton"]')
+        // Update renderer with value of radio button
+        console.log(this.id + ':' + radioButton.name + ' radio button change');
+      });
+
+      // Nest the radio button directly inside the calcite-label
+      radioButtonLabel.appendChild(radioButton);
+      radioButtonLabel.appendChild(document.createTextNode(option || option));
+
+      container.appendChild(radioButtonLabel);
+    });
+  }
+
+  createCheckBoxes(container) {
+    this.options.forEach((option, index) => {
+
+      // create checkboxes
+      var checkBoxLabel = document.createElement("calcite-label");
+      checkBoxLabel.setAttribute('layout', 'inline');
+      checkBoxLabel.classList.add('pointer-cursor');
+
+      var checkBox = document.createElement("calcite-checkbox");
+      checkBox.name = this.id;
+      checkBox.value = option;
+
+      // Optionally, select the first checkbox by default
+      if (option === this.selectedOption) {
+        checkBox.checked = true;
+      }
+      // Listen for changes to the checkbox
+      checkBox.addEventListener("change", function (e) {
+        // to make sure the checkbox is the is the actual element
+        const checkBox = e.currentTarget; // or e.target.closest('input[type="checkbox"]')
+        // update renderer with value of checkbox
+        console.log(this.id + ':' + this.name + ' checkbox change')
+      });    
+
+      // Nest the checkbox directly inside the calcite-label
+      checkBoxLabel.appendChild(checkBox);
+      checkBoxLabel.appendChild(document.createTextNode(option || option));
+
+      container.appendChild(checkBoxLabel);
+
+    });
+  }
+
+  createSelect(container){
+    const select = document.createElement('calcite-select');
+    this.options.forEach(option => {
+      const optionEl = document.createElement('calcite-option');
+      optionEl.value = option;
+      optionEl.textContent = option;
+      
+      if (option === this.selectedOption) {
+        optionEl.setAttribute('selected', 'true'); // This will select the option
+      }
+      select.appendChild(optionEl);
+    });
+    select.addEventListener('change', (e) => {
+      this.selectedOption = e.detail;
+    });
+    container.appendChild(select);
+  }
+
+
+}
---
+++ _site/scripts.js
@@ -0,0 +1,107 @@
+let globalTemplates = [];
+
+require(["esri/config",
+         "esri/Map",
+         "esri/views/MapView",
+         "esri/Basemap",
+         "esri/widgets/BasemapToggle",
+         "esri/layers/GeoJSONLayer",
+         "esri/widgets/Home",
+         "esri/widgets/Search",
+         "esri/layers/TileLayer",
+         "esri/Graphic",
+         "esri/geometry/Point",
+         "esri/geometry/Polygon",
+         "esri/geometry/Polyline",
+         "esri/layers/FeatureLayer",
+         "esri/widgets/LayerList",
+         "esri/renderers/ClassBreaksRenderer",
+         "esri/renderers/UniqueValueRenderer",
+         "esri/renderers/SimpleRenderer",
+         "esri/widgets/Legend",
+         "esri/PopupTemplate",
+         "esri/symbols/TextSymbol",
+         "esri/rest/support/Query",
+         "esri/WebMap"
+        ],
+function(esriConfig, Map, MapView, Basemap, BasemapToggle, GeoJSONLayer, Home, Search, TileLayer, Graphic, Point, Polygon, Polyline, FeatureLayer, LayerList, ClassBreaksRenderer, UniqueValueRenderer, SimpleRenderer, Legend, PopupTemplate, TextSymbol, Query, WebMap) {
+
+  esriConfig.apiKey = "AAPK5f27bfeca6bb49728b7e12a3bfb8f423zlKckukFK95EWyRa-ie_X31rRIrqzGNoqBH3t3Chvz2aUbTKiDvCPyhvMJumf7Wk";
+
+
+
+  async function fetchMenuData() {
+    const response = await fetch('menu.json');
+    const data = await response.json();
+    return data;
+  }
+
+  async function loadMenuAndItems() {
+    const menuJson = await fetchMenuData();
+    const menuData = menuJson.map(item => new MenuItem(item));
+    
+    const calciteMenu = document.querySelector('calcite-menu[slot="content-start"]');
+
+    // Clear existing menu items
+    calciteMenu.innerHTML = '';
+
+    // Render each menu item and log (or insert into the DOM)
+    menuData.forEach(menuItem => {
+      calciteMenu.appendChild(menuItem.createMenuItemElement());
+    });
+  }
+
+  async function init() {
+    const menuStructure = await loadMenuAndItems();
+  }
+
+  function populateTemplates() {
+    const container = document.getElementById('main'); // Assuming your templates will be children of a div with the id "main".
+
+    globalTemplates.forEach(template => {
+        const div = document.createElement('div');
+        div.id = template.templateType + 'Template'; 
+        div.classList.add('template');
+        div.hidden = true;
+        div.innerHTML = template.layoutDivs;
+        container.appendChild(div);
+    });
+
+    document.getElementById('toggleOverlay').addEventListener('click', function() {
+      var overlay = document.getElementById('overlay');
+      if (overlay.classList.contains('collapsed')) {
+        overlay.classList.remove('collapsed');
+        this.innerText = 'Collapse';
+      } else {
+        overlay.classList.add('collapsed');
+        this.innerText = 'Expand';
+      }
+    });
+
+    const map = new Map({
+      basemap: "gray-vector" // Basemap layerSegments service
+    });
+  
+    view = new MapView({
+      map: map,
+      center: [-111.8910, 40.7608], // Longitude, latitude
+      zoom: 10, // Zoom level
+      container: "mapView" // Div element
+    });
+
+    init();
+  }
+
+  fetch('templates.json')
+  .then(response => response.json())
+  .then(data => {
+    globalTemplates = data;
+    // Optionally, initialize your classes or do other tasks here, 
+    // once the data is fetched and assigned.
+    populateTemplates();
+  })
+  .catch(error => {
+    console.error("Error fetching templates:", error);
+  });
+
+});
