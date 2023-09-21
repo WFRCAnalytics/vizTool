ca55fcc920163e3245731ae63627e5a2d9b57b14
Chris Day <christophersday@gmail.com>
2023-09-21 12:52:36 -0600

    Add matrix table

--- _site/classes/model-entity.js
+++ _site/classes/model-entity.js
@@ -48,6 +48,7 @@ class modelEntity {
       modelEntityInstance.populateSidebar(sidebarSelect);  // Use the saved instance context here as well
       modelEntityInstance.populateText();
       modelEntityInstance.populateImage();
+      modelEntityInstance.displayJSONData();
       modelEntityInstance.updateLayerVisibility();
       //modelEntityInstance.populateMainContent(modelEntityInstance.templateContent);
 
@@ -141,7 +142,67 @@ class modelEntity {
         .catch(error => {
             console.error("Error fetching or displaying image:", error);
         });
+  }
+
+  // Function to create and populate the table
+  displayJSONData() {
+    const jsonData = {
+      "data": [
+        [0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123],
+        [0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456],
+        [0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789],
+        [0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321],
+        [0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654],
+        [0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987],
+        [0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135],
+        [0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468],
+        [0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791],
+        [0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123],
+        [0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456],
+        [0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789],
+        [0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321],
+        [0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654],
+        [0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987],
+        [0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135],
+        [0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468],
+        [0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791],
+        [0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123, 0.456, 0.789, 0.321, 0.654, 0.987, 0.135, 0.468, 0.791, 0.123]
+      ]
+    }
+
+    const table = document.getElementById('matrixTable');
+
+    // Create the header row for columns on row 1
+    const headerRow = table.insertRow(0); // Insert at row 0
+    headerRow.classList.add('header-row');
+
+    // Create an empty cell for the row header column
+    headerRow.insertCell();
+
+    // Loop to create column headers with numbers
+    for (let col = 0; col < jsonData.data[0].length; col++) {
+        const headerCell = headerRow.insertCell();
+        headerCell.textContent = `j ${col + 1}`; // Column numbers start from 1
+    }
+
+    // Loop through the rows and columns of the JSON data
+    for (let rowIndex = 0; rowIndex < jsonData.data.length; rowIndex++) {
+        const row = jsonData.data[rowIndex];
+        const newRow = table.insertRow();
+
+        // Create the cell in the first column for the row header
+        const rowHeaderCell = newRow.insertCell();
+        rowHeaderCell.textContent = `i ${rowIndex + 1}`; // Row numbers start from 1
+        rowHeaderCell.classList.add('row-header'); // Apply the row header style
+
+        // Loop through the data cells for this row, starting from the second column
+        for (let colIndex = 0; colIndex < row.length; colIndex++) {
+            const newCell = newRow.insertCell();
+            newCell.textContent = row[colIndex].toFixed(3); // Format the number to show 3 decimal places
+        }
+    }
 }
+
 
 
   getSidebarSelector(submenuTemplate) {
