5e98df7bd32cdb85cdc01823aa9ea60ca99586f5
Bill Hereth <bhereth@wfrc.org>
2024-07-31 15:32:01 -0600

    Start integrating aggregators automation

--- _site/classes/layouts/aggregator.js
+++ _site/classes/layouts/aggregator.js
@@ -5,13 +5,27 @@ class Aggregator {
         this.agCodeNameField = data.agCodeNameField;
         this.agOptions = data.agOptions;
         this.agGeoJson = data.agGeoJson;
-        this.selected = data.agSelected
+        this.agGeoJsonLabelField = data.agGeoJsonLabelField,
+        this.agGeoJsonValueField = data.agGeoJsonValueField,
+        this.selected = data.agSelected ? data.agSelected : [];
     
+        let _options;
+
+        // only run if it is vizTrends and has an agGeoJson defined
+        if (this.agGeoJson) {
+          _options = dataGeojsons[this.agGeoJson].features.map(feature => ({
+            value: String(feature.properties[this.agGeoJsonValueField]), // Assuming these are under `properties`
+            label: feature.properties[this.agGeoJsonLabelField]  // Adjust if they are located elsewhere
+          }));
+        } else {
+          _options = this.agOptions;
+        }
+
         this.filterData = {
           fCode           : this.agCode          ,
           fName           : this.agDisplayName   ,
           fWidget         : "checkboxes"         ,
-          fOptions        : this.agOptions       ,
+          fOptions        : _options             ,
           fSelected       : this.selected        ,
           subAgDisplayName: data.subAgDisplayName,
           subAgSelected   : data.subAgSelected   ,
